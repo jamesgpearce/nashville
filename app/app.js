2da7073a6d642710d1bc8e00e8236a24f3686a34
James Pearce <github@tripleodeon.com>
2011-09-13 21:01:47 -0700

    2_layout

--- app/app.js
+++ app/app.js
@@ -1,13 +1,35 @@
 nv = new Ext.Application({
-
     launch: function() {
-        this.viewport = new Ext.Panel({
 
+        this.listCardToolbar = new Ext.Toolbar({
+            dock: 'top',
+            title: 'Nashville Guide'
+        });
+
+        this.listCardDataList = new Ext.List({
+            store: null,
+            itemTpl: ''
+        });
+
+        this.listCard = new Ext.Panel({
+            dockedItems: [this.listCardToolbar],
+            items: [this.listCardDataList]
+        });
+
+        this.detailCardToolbar = new Ext.Toolbar({
+            dock: 'top',
+            title: '...'
+        });
+
+        this.detailCard = new Ext.Panel({
+            dockedItems: [this.detailCardToolbar]
+        });
+
+        nv.viewport = new Ext.Panel({
             layout: 'card',
             fullscreen: true,
-            html: "Hello world!"
-
+            cardSwitchAnimation: 'slide',
+            items: [this.listCard, this.detailCard]
         });
     }
-
 });
